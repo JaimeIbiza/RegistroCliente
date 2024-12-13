42946a2fa5bafbd9d6cb64648502e97c82ad244c
JaimeIbiza <pauwerisam@gmail.com>
2024-12-13 16:25:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const cors = require('cors');
 const admin = require('firebase-admin');
 
 // Inicializar Firebase con las credenciales
-const serviceAccount = require('./path/to/your/firebase-key.json');
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://<your-project-id>.firebaseio.com"
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+    }),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
 
 const db = admin.firestore();
